5e2b62b273bb2367c01b2c2aa574a3e1e14cdeb7
Pulkit Kasera <pulkitkks@gmail.com>
2017-09-07 01:12:28 +0530

    Form Completed

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -5,4 +5,30 @@ end
 def show
 @article = Article.find(params[:id])
 end
+def new
+@article = Article.new
+end
+def create
+@article = Article.new
+@article.title = params[:article][:title]
+@article.body = params[:article][:body]
+@article.save
+redirect_to article_path(@article)
+end
+def destroy
+@article = Article.find(params[:id])
+@article.destroy
+redirect_to articles_path
+end
+def edit
+@article = Article.find(params[:id])
+end
+def update
+@article = Article.find(params[:id])
+@article.title = params[:article][:title]
+@article.body = params[:article][:body]
+@article.save
+flash.notice = "Article '#{@article.title}' Updated!"
+redirect_to article_path(@article)
+end
 end
